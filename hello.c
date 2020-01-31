baf9bf27757b08fe790ad752659426b36678fe95
sinharitika009 <60056222+sinharitika009@users.noreply.github.com>
2020-01-31 12:14:16 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void printName();
+int main(){
+void printName(int x,char a[20]){
+    printf("%d\n",x);
+    printf("%s",a);
+        
+    }
+    printName(3,"Bandarr");
+    return 0;
+}
